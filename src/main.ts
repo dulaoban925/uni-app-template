2c91b019160199a3fef787c543fedd85e1622fbe
SuperYing <13975145507@163.com>
2022-12-09 09:45:31 +0800

    feat: 集成 pinia

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { createSSRApp } from 'vue'
 import App from './App.vue'
+import * as Pinia from 'pinia'
+
 export function createApp() {
   const app = createSSRApp(App)
+  const store = Pinia.createPinia()
+  app.use(store)
+
   return {
-    app
+    app,
+    Pinia
   }
 }
